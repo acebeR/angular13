caccd37dd0af44d9914b692e399aaafa3101fdd5
aceber <rebecadivinapc@gmail.com>
2023-08-26 12:13:31 -0300

    diretiva - onKeyip

--- conver-de-moedas/src/app/conversor/directives/numero.directive.ts
+++ conver-de-moedas/src/app/conversor/directives/numero.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
-
+import { Directive, HostListener } from '@angular/core';
+import { NG_VALUE_ACCESSOR,ControlValueAccessor } from '@angular/forms'
 @Directive({
-  selector: '[numero]'
+  selector: '[numero]',
+  providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: NumeroDirective,multi: true }]
 })
-export class NumeroDirective {
+export class NumeroDirective implements ControlValueAccessor{
+  onTouches: any;
+  OnChange: any;
+  constructor( private el: ElementRef) { }
+  @HostListener('keyup',['$event'])
+  onKeyUp($event: any){
+    let valor = $event.target.value;
+    let posDecimais = valor.indexOf('.');
 
-  constructor() { }
+    valor = valor.replace(/[\D]/g,'');
 
+    if(posDecimais > 0){
+      valor = valor.substr(0,posDecimais) + '.' + valor.substr(posDecimais);
+    }
+
+    $event.target.value = valor;
+    this.OnChange(valor);
+  }
 }
